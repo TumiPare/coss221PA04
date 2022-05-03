499f3ebc35f53c303af85caf0fca046e72ff68ac
GD Radloff <danielrad082@gmail.com>
2022-05-03 20:42:28 +0200

    The ui is now working. The sql should work but i still need to do some testing

--- prac04/src/main/java/za/ac/up/cs/cos221/Database.java
+++ prac04/src/main/java/za/ac/up/cs/cos221/Database.java
@@ -6,6 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.sql.Types;
 import java.sql.ResultSetMetaData;
 
 import javax.swing.BoxLayout;
@@ -17,7 +18,7 @@ import javax.swing.table.DefaultTableModel;
 
 public class Database {
     protected Connection connection = null;
-  
+
     public Database(String driver,String host,int port,String database,String username,String password)
     {
         String url = new StringBuilder()
@@ -30,10 +31,10 @@ public class Database {
         }
         catch (SQLException e) {
             throw new Error("Error: " + e.getMessage());
-        } 
-     
+        }
+
     }
- 
+
     public ResultSet select(String query)
     {
         try {
@@ -45,7 +46,7 @@ public class Database {
             throw new Error("Error: " + e.getMessage());
         }
     }
- 
+
     public PreparedStatement prepareStatement(String query)
     {
         try {
@@ -64,9 +65,9 @@ public class Database {
           }
     }
     public DefaultTableModel getTableModel(String query) throws SQLException{
-        ResultSet rs = select(query);   
+        ResultSet rs = select(query);
         DefaultTableModel tableModel = new DefaultTableModel();
-        ResultSetMetaData metaData = rs.getMetaData(); 
+        ResultSetMetaData metaData = rs.getMetaData();
         int columnCount = metaData.getColumnCount();
 
         for (int i = 1; i <= columnCount; i++){
@@ -75,13 +76,13 @@ public class Database {
 
         Object[] row = new Object[columnCount];
 
-        while (rs.next()){       
+        while (rs.next()){
             for (int i = 0; i < columnCount; i++){
                 row[i] = rs.getObject(i+1);
-            }     
+            }
             tableModel.addRow(row);
         }
-        return tableModel;     
+        return tableModel;
     }
     public JPanel createForm(String [] labels){
         JPanel mainPnl = new JPanel();
@@ -95,8 +96,119 @@ public class Database {
             pnlRow.add(label);
             pnlRow.add(textField);
             mainPnl.add(pnlRow);
-            label.setLabelFor(textField);       
+            label.setLabelFor(textField);
         }
         return mainPnl;
+    }
+    public boolean insertFilm(String title, String desc, int release,
+	String lang, int duration, Double rate, int length, Double cost,
+	String rating, String originalLang, String[] features) {
+      System.out.println(title);
+      System.out.println(desc);
+      System.out.println(release);
+      System.out.println(lang);
+      System.out.println(duration);
+      System.out.println(rate);
+      System.out.println(length);
+      System.out.println(cost);
+      System.out.println(rating);
+      System.out.println(originalLang);
+      System.out.println(features);
+      // This is just capitalizing the language Strings
+      lang = lang.substring(0, 1).toUpperCase() + lang.substring(1);
+      if (!(originalLang.isEmpty())) {
+	originalLang = originalLang.substring(0, 1).toUpperCase() +
+	  originalLang.substring(1);
+      }
+      // this is setting all the querys we could use.
+      int primaryLanguageID = -1;
+      int secondLanguageKey = -1;
+      String languageQuery = "SELECT language_id FROM language WHERE name = ?";
+      String insertLanguage = "INSERT INTO langauge (name) VALUES (?)";
+      // this fat mess will add a language if it does not exist and if it does
+      // then it will give the key. in the end we get the key either way.
+      try {
+	PreparedStatement prepare = this.connection.prepareStatement(languageQuery);
+	prepare.setString(1, lang);
+	ResultSet results = prepare.executeQuery();
+	if (results.next()) {
+	  primaryLanguageID = results.getInt(1);
+	} else {
+	  prepare = this.connection.prepareStatement(insertLanguage);
+	  prepare.setString(1,lang);
+	  prepare.executeUpdate();
+	  results = prepare.getGeneratedKeys();
+	  if (results.next()) {
+	    primaryLanguageID = results.getInt(1);
+	  }
+	}
+      } catch (Exception e) {
+	return false;
+      }
+      if (!(originalLang.isEmpty())) {
+	try {
+	  PreparedStatement prepare = this.connection.prepareStatement(languageQuery);
+	  prepare.setString(1, originalLang);
+	  ResultSet results = prepare.executeQuery();
+	  if (results.next()) {
+	    secondLanguageKey = results.getInt(1);
+	  } else {
+	    prepare = this.connection.prepareStatement(insertLanguage);
+	    prepare.setString(1,originalLang);
+	    prepare.executeUpdate();
+	    results = prepare.getGeneratedKeys();
+	    if (results.next()) {
+	      secondLanguageKey = results.getInt(1);
+	    }
+	  }
+	} catch (Exception e) {
+	  return false;
+	}
+      }
+      // end of language validation.
+      String insertQuery = "INSERT INTO film (title, description, release_year," +
+	     "language_id, original_language_id, rental_duration, rental_rate," +
+	     "length, replacement_cost, rating, special_features) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+      try {
+	PreparedStatement prepare = this.connection.prepareStatement(insertQuery);
+	prepare.setString(1, title);
+	if (desc.isEmpty()) {
+	  prepare.setNull(2, Types.NULL);
+	} else {
+	  prepare.setString(2, desc);
+	}
+	if (release == -1) {
+	  prepare.setNull(3, Types.NULL);
+	} else {
+	  prepare.setString(3, String.valueOf(release));
+	}
+	prepare.setInt(4, primaryLanguageID);
+	if (originalLang.isEmpty()) {
+	  prepare.setNull(5, Types.NULL);
+	} else {
+	  prepare.setInt(5, secondLanguageKey);
+	}
+	prepare.setInt(6, duration);
+	prepare.setDouble(7, rate);
+	if (length == -1) {
+	  prepare.setNull(8, Types.NULL);
+	} else {
+	  prepare.setInt(8,length);
+	}
+	prepare.setDouble(9,cost);
+	prepare.setString(10,rating);
+	String newstrarray = "(";
+	for (int count = 0; count < features.length-1; count++) {
+	  newstrarray += features[count] + ",";
+	}
+	if (features.length != 0) {
+	  newstrarray += features[features.length-1] + ")";
+	}
+	prepare.setString(11,newstrarray);
+	prepare.executeUpdate();
+      } catch (Exception e) {
+	return false;
+      }
+      return true;
     }
 }
--- prac04/src/main/java/za/ac/up/cs/cos221/FilmsForm.java
+++ prac04/src/main/java/za/ac/up/cs/cos221/FilmsForm.java
@@ -2,25 +2,241 @@
 // This is called from FilmsPanel.
 package za.ac.up.cs.cos221;
 
-import javax.swing.BoxLayout;
-import javax.swing.JDialog;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.*;
 // import javax.swing.SpringLayout;
 // import java.awt.Frame;
 
 // import java.awt.Container;
 import java.awt.Dimension;
-public class FilmsForm extends JDialog {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class FilmsForm extends JDialog implements ActionListener {
+    private JButton btnForm = new JButton("Submit");
+    private JLabel heading;
+    private JLabel lTitle;
+    private JTextField iTitle;
+
+    private JLabel lDescription;
+    private JTextField iDescription;
+
+    private JLabel lRelease;
+    private JTextField iRelease;
+
+    private JLabel lLanguage;
+    private JTextField iLanguage;
+    private JLabel loLanguage;
+    private JTextField ioLanguage;
+
+    private JLabel lRental;
+    private JTextField iRental;
+
+    private JLabel lRate;
+    private JTextField iRate;
+
+    private JLabel lLenght;
+    private JTextField iLenght;
+
+    private JLabel lCost;
+    private JTextField iCost;
+
+    private JLabel lRating;
+    private JComboBox<String> iRating;
+
+    private JCheckBox [] ifeatures;
+    private Database db;
     public FilmsForm(JFrame parent,Database db){
-        super(parent,"Enter data",true);
-        String[] labels = {"Title: ", "Description: ", "Release Year: ", "Language: ",
-                            "Rental Duration: ", "Rental Rate: ","Length: ","Cost: ",
-                            "Rating: ", "Last Update: "};
-        JPanel mainPnl = db.createForm(labels);
-        this.add(mainPnl);
-        this.pack();
+      // This all builds the visual form and sets the DB to a class variable.
+      super(parent,"Enter data",true);
+      this.db = db;
+      this.setLayout(null);
+      this.setPreferredSize(new Dimension(300,560));
+      this.setSize(300,400);
+
+      heading = new JLabel("Add a rental");
+      heading.setSize(300,30);
+      heading.setLocation(100,30);
+      this.add(heading);
+
+      lTitle = new JLabel("Title");
+      lTitle.setLocation(0, 100);
+      lTitle.setSize(100,20);
+      this.add(lTitle);
+
+      iTitle = new JTextField();
+      iTitle.setLocation(100, 100);
+      iTitle.setSize(190, 20);
+      this.add(iTitle);
+
+      lDescription = new JLabel("Description");
+      lDescription.setLocation(0,130);
+      lDescription.setSize(100,20);
+      this.add(lDescription);
+
+      iDescription = new JTextField();
+      iDescription.setLocation(100,130);
+      iDescription.setSize(190,20);
+      this.add(iDescription);
+
+      lRelease = new JLabel("Release year: ");
+      lRelease.setLocation(0,160);
+      lRelease.setSize(100,20);
+      this.add(lRelease);
+
+      iRelease = new JTextField();
+      iRelease.setLocation(100,160);
+      iRelease.setSize(190,20);
+      this.add(iRelease);
+
+      lLanguage = new JLabel("Language: ");
+      lLanguage.setLocation(0,190);
+      lLanguage.setSize(100,20);
+      this.add(lLanguage);
+
+      iLanguage = new JTextField();
+      iLanguage.setLocation(100,190);
+      iLanguage.setSize(190,20);
+      this.add(iLanguage);
+
+      lRental = new JLabel("Rental Duration(days): ");
+      lRental.setLocation(0, 220);
+      lRental.setSize(100,20);
+      this.add(lRental);
+
+      iRental = new JTextField();
+      iRental.setLocation(100,220);
+      iRental.setSize(190,20);
+      this.add(iRental);
+
+      lRate = new JLabel("Rental Rate(number): ");
+      lRate.setLocation(0,250);
+      lRate.setSize(100,20);
+      this.add(lRate);
+
+      iRate = new JTextField();
+      iRate.setLocation(100,250);
+      iRate.setSize(190,20);
+      this.add(iRate);
+
+      lLenght = new JLabel("Length(minutes): ");
+      lLenght.setLocation(0,280);
+      lLenght.setSize(100,20);
+      this.add(lLenght);
+
+      iLenght = new JTextField();
+      iLenght.setLocation(100,280);
+      iLenght.setSize(190,20);
+      this.add(iLenght);
+
+
+      lCost = new JLabel("Cost(number): ");
+      lCost.setLocation(0,310);
+      lCost.setSize(100,20);
+      this.add(lCost);
+
+      iCost = new JTextField();
+      iCost.setLocation(100,310);
+      iCost.setSize(190,20);
+      this.add(iCost);
+
+
+      String [] rateoptions = {"G","PG","PG-13", "R", "NC-17"};
+      lRating = new JLabel("Rating: ");
+      lRating.setLocation(0,340);
+      lRating.setSize(100,20);
+      this.add(lRating);
+
+      iRating = new JComboBox<>(rateoptions);
+      iRating.setLocation(100,340);
+      iRating.setSize(190,20);
+      this.add(iRating);
+
+      loLanguage = new JLabel("Original Language: ");
+      loLanguage.setLocation(0,370);
+      loLanguage.setSize(100,20);
+      this.add(loLanguage);
+
+      ioLanguage = new JTextField();
+      ioLanguage.setLocation(100,370);
+      ioLanguage.setSize(190,20);
+      this.add(ioLanguage);
+
+      String [] features = { "Trailers", "Commentaries", "Deleted Scenes",
+	"Behind the Scenes"};
+      ifeatures = new JCheckBox[features.length];
+      for (int count = 0; count < features.length; count++) {
+	ifeatures[count] = new JCheckBox(features[count]);
+	ifeatures[count].setLocation(150*(count%2),400+(30*(int)Math.floor(count/2)));
+	ifeatures[count].setSize(150,20);
+	this.add(ifeatures[count]);
+      }
+
+      btnForm.addActionListener(this);
+      btnForm.setLocation(100,490);
+      btnForm.setSize(100,40);
+      this.add(btnForm);
+      this.pack();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent arg0) {
+	// handler for the button click event.
+        if(arg0.getSource()==this.btnForm){
+	  try {
+	    if (this.iTitle.getText().isEmpty()) {
+	      throw new Exception("You need a title");
+	    }
+	    if (this.iLanguage.getText().isEmpty()) {
+	      throw new Exception("You must have a language");
+	    }
+	    int year, duration, length;
+	    Double rate, cost;
+	    if (this.iRelease.getText().length() > 0) {
+	      year = Integer.parseInt(this.iRelease.getText());
+	      if (!(year >= 1000 && year <=9999)) {
+		throw new Exception("We don't have a time machine :/ please enter a valid year for a movie release date"); } } else {year = -0;}
+	    if (this.iRental.getText().length() > 0) {
+	      duration = Integer.parseInt(this.iRental.getText());
+	    } else {duration = 3;}
+	    if (this.iRate.getText().length() > 0) {
+	      rate = Double.parseDouble(this.iRate.getText());
+	    } else {rate = 4.99;}
+	    if (this.iLenght.getText().length() > 0) {
+	      length = Integer.parseInt(this.iLenght.getText());
+	    } else {length = -0;}
+	    if (this.iCost.getText().length() > 0) {
+	      cost = Double.parseDouble(this.iCost.getText());
+	    } else {cost = 19.99;}
+	    // this code is trash but it works very well and it does what it
+	    // needs to do. I don't feel like linked lists right now.
+	    int extcount = 0;
+	    for (int count = 0; count < this.ifeatures.length; count++) {
+	      if (this.ifeatures[count].isSelected()) {
+		extcount++;
+	      }
+	    }
+	    String[] features = new String[extcount];
+	    extcount = 0;
+	    for (int count = 0; count < this.ifeatures.length; count++) {
+	      if (this.ifeatures[count].isSelected()) {
+		features[extcount] = ifeatures.toString();
+		extcount++;
+	      }
+	    }
+	    boolean worked = db.insertFilm(this.iTitle.getText(), this.iDescription.getText(),
+		year, this.iLanguage.getText(), duration, rate, length, cost,
+		this.iRating.getItemAt(this.iRating.getSelectedIndex()), this.ioLanguage.getText(),
+		features);
+	    if (!worked) {
+	    JOptionPane.showMessageDialog(this, "Something whent wrong while trying to add the entry to the database.... Sorry for that..",
+                                   "ERROR", JOptionPane.ERROR_MESSAGE);
+	    }
+	  } catch(NumberFormatException e) {
+	    JOptionPane.showMessageDialog(this, "Please check that fields that must be numbers are numbers.",
+                                   "ERROR", JOptionPane.ERROR_MESSAGE);
+	  } catch(Exception e) {
+	    JOptionPane.showMessageDialog(this, e.getMessage(),
+                                   "ERROR", JOptionPane.ERROR_MESSAGE);
+	  }
+	}
     }
 }
